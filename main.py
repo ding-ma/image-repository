0ce1f74a0956c4502c53f1f3455941a21fc6357f
ding-ma <ding.ma@mail.mcgill.ca>
2021-05-03 15:48:43 -0400

    returns basic url from vision api

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+
 from dotenv import load_dotenv
 from google.cloud import vision
 import io
@@ -14,24 +17,55 @@ def index():
     return render_template('index.html')
 
 
+def search_by_image(img):
+    response = client.label_detection(image=img)
+    print(response)
+    labels = response.label_annotations
+
+    print('Labels:')
+    label_found = []
+    for label in labels:
+        print(label.description)
+        label_found.append(
+            f"https://www.google.com/search?q={label.description}")
+    return set(label_found)
+
+
+def search_by_text(img):
+    response = client.text_detection(image=img)
+    texts = response.text_annotations
+    print('Texts:')
+
+    text_found = []
+    for text in texts:
+        for t in text.description.split("\n"):
+            if t == '':
+                continue
+            text_found.append(f"https://www.google.com/search?q={t}")
+    return set(text_found)
+
+
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     option = request.form.getlist('options')
 
     print(option, uploaded_file)
+    if len(option) == 0 or uploaded_file.filename == '':
+        return render_template('error.html')
+
+    uploaded_file.save(secure_filename(uploaded_file.filename))
+
     with io.open(uploaded_file.filename, 'rb') as image_file:
         content = image_file.read()
 
     image = vision.Image(content=content)
-    response = client.label_detection(image=image)
-    print(response)
-    labels = response.label_annotations
+    if option[0] == "text":
+        ans = search_by_text(image)
+    else:
+        ans = search_by_image(image)
 
-    print('Labels:')
-    for label in labels:
-        print(label.description)
-    return redirect(url_for('index'))
+    return render_template('index.html', data=ans)
 
 
 if __name__ == '__main__':
